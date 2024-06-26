e2d3677587e17c3ab7f9b313ce45ef33c73ee307
Aleksei Terin <aleksei.terin@outlook.com>
2024-06-27 01:42:02 +0300

    expo: initial commit

---
+++ frontend/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
