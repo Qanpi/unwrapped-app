61d9a8a972611665ee8d5a1d6927872f87ae2f2f
Aleksei Terin <aleksei.terin@outlook.com>
2024-06-29 22:12:35 +0300

    wrapped cards
    - scroll view layout
    - auto generation from assets

---
+++ expo-router/app/chat/Decoration.tsx
@@ -0,0 +1,89 @@
+import { useAssets } from "expo-asset";
+import { ReactNode } from "react";
+import { Image } from "tamagui";
+
+export function Decoration({ asset, x, y, size = 150 }) {
+  const angle = Math.floor(Math.random() * 360);
+
+  const height = size,
+    width = size;
+
+  return (
+    <Image
+      source={asset}
+      width={width}
+      height={height}
+      position="absolute"
+      objectFit="contain"
+      resizeMode="contain"
+      top={y}
+      left={x}
+      transform={[
+        { translateX: -width / 2 },
+        { translateY: -height / 2 },
+        { rotateZ: `${angle}deg` },
+      ]}
+    ></Image>
+  );
+}
+
+const clamp = (x: number, lo: number, hi: number) =>
+  Math.max(Math.min(x, hi), lo);
+
+export const useCornerDecorations = (
+  width: number,
+  height: number,
+  count: number
+) => {
+  const DECORATION_COUNT = 8;
+
+  //order is chosen so that at least one 3D rainbow element is present
+  const [assets, errors] = useAssets([
+    require(`../../assets/images/decorations/cloud.png`), //3D
+    require(`../../assets/images/decorations/red.png`),
+    require(`../../assets/images/decorations/metal.png`),
+    require(`../../assets/images/decorations/stripe.png`),
+    require(`../../assets/images/decorations/flower.png`), //3D
+    require(`../../assets/images/decorations/spill.png`),
+    require(`../../assets/images/decorations/signature.png`),
+    require(`../../assets/images/decorations/palm.png`),
+  ]);
+
+  if (!assets) return [];
+
+  //TODO: match order across cards?
+  const randomAssetId = Math.floor(Math.random() * DECORATION_COUNT);
+
+  const decorations: ReactNode[] = [];
+
+  //TODO: add RNG seeding
+  for (let i = 0; i < count; i++) {
+    const columns = 6;
+    const rows = 8;
+
+    const column = Math.floor(width / columns);
+    const row = Math.floor(height / rows);
+
+    const inset = 20;
+    const minX = clamp((i % 2) * column * (columns - 1), inset, width - inset);
+    const minY = clamp(
+      Math.floor(i / 2) * row * (rows - 1),
+      inset,
+      height - inset
+    );
+
+    const maxX = minX + column;
+    const maxY = minY + row;
+
+    decorations.push(
+      <Decoration
+        key={i}
+        asset={assets[(randomAssetId + i) % DECORATION_COUNT]}
+        x={Math.random() * (maxX - minX) + minX}
+        y={Math.random() * (maxY - minY) + minY}
+      ></Decoration>
+    );
+  }
+
+  return decorations;
+};
