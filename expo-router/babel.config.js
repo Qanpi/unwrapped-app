c2f1420a6084239b5cb7f87ce7713a2f924b62ce
Aleksei Terin <aleksei.terin@outlook.com>
2024-07-06 01:26:11 +0300

    personal stats

--- expo-router/babel.config.js
+++ expo-router/babel.config.js
@@ -1,20 +1,21 @@
 module.exports = (api) => {
-  api.cache(true)
+  api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [["babel-preset-expo", { jsxRuntime: "automatic" }]],
+
     plugins: [
       [
-        '@tamagui/babel-plugin',
+        "@tamagui/babel-plugin",
         {
-          components: ['tamagui'],
-          config: './tamagui.config.ts',
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
           logTimings: true,
-          disableExtraction: process.env.NODE_ENV === 'development',
+          disableExtraction: process.env.NODE_ENV === "development",
         },
       ],
 
       // NOTE: this is only necessary if you are using reanimated for animations
-      'react-native-reanimated/plugin',
+      "react-native-reanimated/plugin",
     ],
-  }
-}
+  };
+};
