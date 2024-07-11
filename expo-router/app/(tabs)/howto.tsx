229597c56e1a3f468b18e02d8dd069e09e780c50
Aleksei Terin <aleksei.terin@outlook.com>
2024-07-11 20:18:16 +0300

    paywall modal route

--- expo-router/app/(tabs)/howto.tsx
+++ expo-router/app/(tabs)/howto.tsx
@@ -1,13 +1,11 @@
 import { H4, Text, View } from "tamagui";
 import { HeaderDropdown } from "./_layout";
 import { DefaultYStack } from ".";
-import { Paywall } from "app/Paywall";
 
 export default function HowToScreen() {
   return (
     <DefaultYStack>
       <H4>Get started</H4>
-      <Paywall></Paywall>
     </DefaultYStack>
   );
 }
